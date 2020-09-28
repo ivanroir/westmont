1404aa6d1b5ef0ff0f004dc7fa295f3ced2db7af
ivanroir <ivanroirbruselas75@gmail.com>
2020-09-28 19:54:11 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -247,7 +247,7 @@ function takeSnapshot() {
   context.save(); 
   context.scale(-1, 1); 
   //context.drawImage(video, width * -1, 0, width, height);
-  context.drawImage(video, width * -1, 0, width, height, width * -1, 0, width, height);
+  context.drawImage(video, 0, 0, width * .75, height, width * -.75, 0, width * .75, height);
   context.restore();
   context.drawImage(frame, 0, 0, width, height);
   
