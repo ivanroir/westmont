708373958080a09179b4208e4b7b8d176c1c63ed
ivanroir <ivanroirbruselas75@gmail.com>
2020-10-04 13:59:20 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -269,11 +269,11 @@ function takeSnapshot(input = null) {
 
     var img = new Image;
     img.src = URL.createObjectURL(input.files[0]);
-    img.onload = function (e) {
+    //img.onload = function (e) {
       var fileImage = document.getElementById("fileImage");
       console.log(fileImage);
       context.drawImage(img, 0, 0, width, height);
-    }
+    //}
     context.drawImage(frame, 0, 0, width, height);
 
   } else {
