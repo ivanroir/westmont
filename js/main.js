f9adbda12123402c1c6761c5cc5fc5e690f1e718
ivanroir <ivanroirbruselas75@gmail.com>
2020-10-11 08:54:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -203,8 +203,8 @@ function initCameraStream() {
   var constraints = {
     audio: false,
     video: {
-      width: { ideal: widthSize },
-      height: { ideal: heightSize },
+      width: { ideal: size },
+      height: { ideal: size },
       //width: { min: 1024, ideal: window.innerWidth, max: 1920 },
       //height: { min: 776, ideal: window.innerHeight, max: 1080 },
       facingMode: currentFacingMode,
