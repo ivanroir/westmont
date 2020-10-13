a67e595ce507f165223e232571a642c1acd68431
ivanroir <ivanroirbruselas75@gmail.com>
2020-10-13 13:28:48 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@ function initCameraStream() {
 
       context = canvas.getContext('2d');
       if (currentFacingMode == "environment"){
-        context.drawImage(video, 0, 0, width * 1.20, height);
+        context.drawImage(video, 0, 0, width * 1.15, height);
       }else{
         context.save(); 
         context.scale(-1, 1); 
