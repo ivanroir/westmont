21f0f5b463cf45cacd269cb759209cc033a52588
ivanroir <ivanroirbruselas75@gmail.com>
2020-10-14 21:23:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -255,12 +255,12 @@ function initCameraStream() {
       context = canvas.getContext('2d');
       if (currentFacingMode == "environment"){
         //context.drawImage(video, -180, 0, width * 1.32, height);
-        context.drawImage(video, 0, -270, width, height * 1.32);
+        context.drawImage(video, 0, -300, width, height * 1.32);
       }else{
         context.save(); 
         context.scale(-1, 1); 
         //context.drawImage(video, (width * -1) - 180, 0, width * 1.32, height);
-        context.drawImage(video, width * -1, -270, width, height * 1.32);
+        context.drawImage(video, width * -1, -300, width, height * 1.32);
         context.restore();
       }
       //context.drawImage(video, 30, 0, width * .75, height, width * -.75, 0, width * .75, height);
