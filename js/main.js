a2ce2d1eb2467983d143436b0fcf3fcf5b2fa50e
unknown <ivanroirbruselas75@gmail.com>
2021-07-11 13:59:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -250,15 +250,16 @@ function initCameraStream() {
       canvas.width = width;
       canvas.height = height;
 
-      console.log(screen.availWidth);
-      console.log(screen.availHeight);
+      console.log(video.width);
+      console.log(video.height);
 
       context = canvas.getContext('2d');
       if (currentFacingMode == "environment"){   
         context.save(); 
         context.scale(-1, 1);         
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-          context.drawImage(video, (screen.availWidth * -1 ), 0, width, height);
+          context.drawImage(video, 0, 0, 100, 100 * video.height / video.width);
+          //context.drawImage(video, (screen.availWidth * -1 ), 0, width, height);
           //context.drawImage(video, (width * -1) + -700, 0, width * 2.1, height);
         }
         else {
