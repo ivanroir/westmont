d25946865206c7b500341241c65faa102309d6bd
unknown <ivanroirbruselas75@gmail.com>
2021-07-11 22:48:14 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -285,7 +285,7 @@ function initCameraStream() {
             context.scale(-1, 1);
             //context.drawImage(video, (width * -1) + -270, 0, width * 1.43, height);
             //context.drawImage(video, 0, 0, width, height, 0, 0, width, height);
-            context.drawImage(video, (width / 2) - 920 , 0, width * 1.8 , height);
+            context.drawImage(video, (width) - 620 , 0, width * 1.8 , height);
             context.restore();
             alert("4");
         }
