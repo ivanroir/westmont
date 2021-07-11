918319a070a1c4fd6556aace276ffcc607accafd
unknown <ivanroirbruselas75@gmail.com>
2021-07-11 18:09:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -263,7 +263,7 @@ function initCameraStream() {
         }
         else if (screen.availHeight < screen.availWidth) {*/
           if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-            context.drawImage(video, 720, 0, width * 2 , height);
+            context.drawImage(video, -720, 0, width * 2 , height);
             //context.drawImage(video, (screen.availWidth * -1 ), 0, width, height);
           }
           else {
