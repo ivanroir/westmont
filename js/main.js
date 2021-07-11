dfad08623fcc4ea425de566057dcd825d2a5d858
unknown <ivanroirbruselas75@gmail.com>
2021-07-11 14:00:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -258,7 +258,7 @@ function initCameraStream() {
         context.save(); 
         context.scale(-1, 1);         
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-          context.drawImage(video, 0, 0, 100, 100 * video.height / video.width);
+          context.drawImage(video, (screen.availWidth * -1 ), 0, 100, 100 * video.height / video.width);
           //context.drawImage(video, (screen.availWidth * -1 ), 0, width, height);
           //context.drawImage(video, (width * -1) + -700, 0, width * 2.1, height);
         }
