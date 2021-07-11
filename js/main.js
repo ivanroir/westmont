e1184d9048009da385bc396a12ff8ef3c92a2f0c
unknown <ivanroirbruselas75@gmail.com>
2021-07-11 18:53:12 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -263,7 +263,7 @@ function initCameraStream() {
         }
         else if (screen.availHeight < screen.availWidth) {*/
           if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-            context.drawImage(video, (width / 2) * - 1, 0, width * (screen.availWidth * .0015 ) , height); // small == shrink img , large == stretch img
+            context.drawImage(video, (width / 2) * - 1, 0, width / (screen.availWidth * .0015 ) , height); // small == shrink img , large == stretch img
             
             //context.drawImage(video, (screen.availWidth * -1 ), 0, width, height);
           }
