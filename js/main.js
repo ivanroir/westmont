ee9b848d616ce9129462401a57fb21e22608e438
unknown <ivanroirbruselas75@gmail.com>
2021-07-11 11:14:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -251,9 +251,12 @@ function initCameraStream() {
       canvas.height = height;
 
       context = canvas.getContext('2d');
-      if (currentFacingMode == "environment"){            
-        context.drawImage(video, 170, 0, width * 1.25, height);                          //portrait size
+      if (currentFacingMode == "environment"){   
+        context.save(); 
+        context.scale(-1, 1);          
+        context.drawImage(video, -170, 0, width * 1.25, height);                          //portrait size
         //context.drawImage(video, 0, -300, width, height * 1.32);
+        context.restore();
       }else{
         context.save(); 
         context.scale(-1, 1); 
