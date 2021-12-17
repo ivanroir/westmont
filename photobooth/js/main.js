4465be09ee511cfcc391df0ec7da0385d9518239
unknown <ivanroirbruselas75@gmail.com>
2021-12-17 11:07:41 +0800

    Update main.js

--- photobooth/js/main.js
+++ photobooth/js/main.js
@@ -273,7 +273,7 @@ function initCameraStream() {
           alert("2");
         }
         else {
-          context.drawImage(video, (width * -1) + -260, 0, width * 1.8, height);
+          context.drawImage(video, (width * -1) + -260, 0, width * 1.5, height);
           //alert("3"); Laptop
         }
       }
